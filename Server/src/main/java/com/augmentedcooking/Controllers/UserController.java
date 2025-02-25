bfdde17ec866f32a282eee80073003d5237a078d
Luka Stojić <124006893+stojic-luka@users.noreply.github.com>
2025-02-25 13:24:03 +0100

    final fix

--- Server/src/main/java/com/augmentedcooking/Controllers/UserController.java
+++ Server/src/main/java/com/augmentedcooking/Controllers/UserController.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package com.augmentedcooking.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,36 +29,3 @@ public class UserController {
         return ResponseWrapper.success(authentication.getName());
     }
 }
-=======
-package com.augmentedcooking.Controllers;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.augmentedcooking.Models.Response.ResponseWrapper;
-import com.augmentedcooking.Services.User.IUserService;
-
-@RestController
-@RequestMapping(path = "/api/v1/user")
-public class UserController {
-
-    private final IUserService userService;
-
-    @Autowired
-    public UserController(IUserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping(path = "/self", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getAllRecipes(UsernamePasswordAuthenticationToken authentication) {
-
-        // testing
-        return ResponseWrapper.success(authentication.getName());
-    }
-}
->>>>>>> Stashed changes
--- Server/src/main/java/com/augmentedcooking/Repositories/Recipe/RecipeRepository.java
+++ Server/src/main/java/com/augmentedcooking/Repositories/Recipe/RecipeRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package com.augmentedcooking.Repositories.Recipe;
 
 import java.util.List;
@@ -64,69 +63,3 @@ public class RecipeRepository implements IRecipeRepository {
         return results.getMappedResults();
     }
 }
-=======
-package com.augmentedcooking.Repositories.Recipe;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.stereotype.Repository;
-
-import com.augmentedcooking.Models.Database.Recipe.Recipe;
-
-@Repository
-public class RecipeRepository implements IRecipeRepository {
-
-    private final MongoTemplate mongoTemplate;
-
-    @Autowired
-    public RecipeRepository(MongoTemplate mongoTemplate) {
-        this.mongoTemplate = mongoTemplate;
-    }
-
-    @Override
-    public List<Recipe> findAll(int page, int limit) {
-        Aggregation aggregation = Aggregation.newAggregation(
-                lookup("recipeImages", "recipeImage", "_id", "image"),
-                skip((long) page * limit),
-                limit(limit));
-
-        AggregationResults<Recipe> results = mongoTemplate.aggregate(aggregation, "recipe", Recipe.class);
-
-        return results.getMappedResults();
-    }
-
-    public Recipe findRandom() {
-        Aggregation aggregation = Aggregation.newAggregation(
-                sample(1),
-                lookup("recipeImages", "recipeImage", "_id", "image"));
-
-        AggregationResults<Recipe> results = mongoTemplate.aggregate(aggregation, "recipe", Recipe.class);
-
-        return results.getMappedResults().get(0);
-    }
-
-    @Override
-    public List<Recipe> findByIngredients(List<String> ingredients, int page, int limit) {
-        Criteria ingredientCriteria = new Criteria();
-        ingredientCriteria.andOperator(
-                ingredients.stream()
-                        .map(ingredient -> Criteria.where("ingredients").regex(".*" + ingredient + ".*", "i"))
-                        .toArray(Criteria[]::new));
-
-        Aggregation aggregation = Aggregation.newAggregation(
-                match(ingredientCriteria),
-                lookup("recipeImages", "recipeImage", "_id", "image"),
-                skip((long) page * limit),
-                limit(limit));
-
-        AggregationResults<Recipe> results = mongoTemplate.aggregate(aggregation, "recipe", Recipe.class);
-        return results.getMappedResults();
-    }
-}
->>>>>>> Stashed changes
